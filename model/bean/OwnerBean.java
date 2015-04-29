580d66008b79ecba3e463c34e333a7317884d651
cweng777 <cweng777@gmail.com>
2015-04-29 15:13:43 +0800

    OwnerBeanUpdate

--- model/bean/OwnerBean.java
+++ model/bean/OwnerBean.java
@@ -1,5 +1,94 @@
 package model.bean;
 
 public class OwnerBean {
+	private Integer ownID;
+    private String ownAcc;
+    private String ownPwd;
+    private String ownEmail;
+    private String ownLastName;
+    private String ownFirstName;
+    private Byte[] ownStatusID;
+    private Byte[] ownSuspend;
+    
+    public OwnerBean() {
+		
+	}
+    
+	@Override
+	public String toString() {
+		return "{"+ownID+","+ownAcc+","+ownPwd+","+ownEmail+","+ownLastName+","+
+				ownFirstName+","+ownStatusID+","+ownSuspend+"}";
+	}
 
+	public Integer getOwnID() {
+		return ownID;
+	}
+
+	public void setOwnID(Integer ownID) {
+		this.ownID = ownID;
+	}
+
+	public String getOwnAcc() {
+		return ownAcc;
+	}
+
+	public void setOwnAcc(String ownAcc) {
+		this.ownAcc = ownAcc;
+	}
+
+	public String getOwnPwd() {
+		return ownPwd;
+	}
+
+	public void setOwnPwd(String ownPwd) {
+		this.ownPwd = ownPwd;
+	}
+
+	public String getOwnEmail() {
+		return ownEmail;
+	}
+
+	public void setOwnEmail(String ownEmail) {
+		this.ownEmail = ownEmail;
+	}
+
+	public String getOwnLastName() {
+		return ownLastName;
+	}
+
+	public void setOwnLastName(String ownLastName) {
+		this.ownLastName = ownLastName;
+	}
+
+	public String getOwnFirstName() {
+		return ownFirstName;
+	}
+
+	public void setOwnFirstName(String ownFirstName) {
+		this.ownFirstName = ownFirstName;
+	}
+
+	public Byte[] getOwnStatusID() {
+		return ownStatusID;
+	}
+
+	public void setOwnStatusID(Byte[] ownStatusID) {
+		this.ownStatusID = ownStatusID;
+	}
+
+	public Byte[] getOwnSuspend() {
+		return ownSuspend;
+	}
+
+	public void setOwnSuspend(Byte[] ownSuspend) {
+		this.ownSuspend = ownSuspend;
+	}
+	
+//    public static void main(String[] args){   //for test
+//    	OwnerBean bean=new OwnerBean();
+//    	bean.setOwnID(12);
+//    	System.out.println(bean);
+//    }
+    
+    
 }

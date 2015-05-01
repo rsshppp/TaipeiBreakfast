6da75266735407f0712d788c8685c026384383e2
PasionDu <pvpms2412@gmail.com>
2015-05-01 21:23:49 +0800

    hbm 3rd
    specialPrice 要改成用 Double

--- model/bean/SpecialPriceBean.java
+++ model/bean/SpecialPriceBean.java
@@ -8,44 +8,43 @@ public class SpecialPriceBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	private Integer SpecialPriceID;
 	private Integer mealID;
 	private Integer specialPrice;
+	//specialPrice 要改成用 Double
 	private Date startDate;
 	private Date endDate;
 	
-	public SpecialPriceBean() {
 
+	public Integer getSpecialPriceID() {
+		return SpecialPriceID;
 	}
-
+	public void setSpecialPriceID(Integer specialPriceID) {
+		SpecialPriceID = specialPriceID;
+	}
 	public Integer getMealID() {
 		return mealID;
 	}
-
 	public void setMealID(Integer mealID) {
 		this.mealID = mealID;
 	}
-
 	public Integer getSpecialPrice() {
 		return specialPrice;
 	}
-
 	public void setSpecialPrice(Integer specialPrice) {
 		this.specialPrice = specialPrice;
 	}
-
 	public Date getStartDate() {
 		return startDate;
 	}
-
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-
 	public Date getEndDate() {
 		return endDate;
 	}
-
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+	
 }

be65308e1c48dd89955d20fa8c6d670b343016f0
steven <rsshppp@gmail.com>
2015-05-06 15:58:28 +0800

    Update HibernateUtil.java by Steven

--- model/misc/HibernateUtil.java
+++ model/misc/HibernateUtil.java
@@ -1,5 +1,39 @@
 package model.misc;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+ 
+public class HibernateUtil {
+ 
+    private static final SessionFactory sessionFactory;
+    private static final StandardServiceRegistry serviceRegistry;
+ 
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+ 
+            serviceRegistry = new StandardServiceRegistryBuilder()
+                               .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+ 
+    public static void closeAllResources() {
+        sessionFactory.close();
+        StandardServiceRegistryBuilder.destroy(serviceRegistry);
+    }
+}
+
+/*
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -30,3 +64,4 @@ public class HibernateUtil {
 	}
 
 }
+*/

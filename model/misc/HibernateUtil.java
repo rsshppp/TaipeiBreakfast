37f696a8ad81a04d3858cc6657d7f706bbba9976
steven <rsshppp@gmail.com>
2015-05-06 18:32:05 +0800

    試著更新

--- model/misc/HibernateUtil.java
+++ model/misc/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
- 
+//by Steven
 public class HibernateUtil {
  
     private static final SessionFactory sessionFactory;

ab85d8afbd2d1033cf9af86cf28a40d9bcd176ea
GarysLin <cylin50@gmail.com>
2015-05-06 19:03:57 +0800

    修正 Meal和AD系列

--- model/bean/AdvertisementStatusBean.java
+++ model/bean/AdvertisementStatusBean.java
@@ -8,6 +8,11 @@ public class AdvertisementStatusBean implements Serializable{
 	private Integer advertisementStatusID;
 	private String advertisementStatus;
 	
+	
+	@Override
+	public String toString() {
+		return "advertisementStatusID="+advertisementStatusID+"，advertisementStatus="+advertisementStatus;
+	}
 	public Integer getAdvertisementStatusID() {
 		return advertisementStatusID;
 	}

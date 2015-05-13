e9d95ca53a1af0e91997397ef987d615e554255e
silverbullet2 <panwangle@gmail.com>
2015-05-13 15:41:16 +0800

    新增訂單明細 - Noah

--- model/dao/OrderDetailDAO.java
+++ model/dao/OrderDetailDAO.java
@@ -2,12 +2,13 @@ package model.dao;
 
 import java.util.List;
 
+import model.bean.MealBean;
 import model.bean.OrderDetailBean;
 import model.bean.OrderSumBean;
 
 public interface OrderDetailDAO {
-	//利用個別總訂單查詢訂單明細，接著查詢餐點名字
-	public List<OrderDetailBean> queryOrderDetails(OrderSumBean bean);
+//	//利用個別總訂單查詢訂單明細，接著查詢餐點名字
+//	public List<OrderDetailBean> queryOrderDetails(OrderSumBean bean);
 	
 	//利用個別總訂單查詢訂單明細，接著查詢餐點名字                                             
 	public List<OrderDetailBean> queryOrderDetails(Integer orderSumID);
@@ -16,4 +17,12 @@ public interface OrderDetailDAO {
 	//查詢交易歷史記錄 -利用此訂單明細內的資料和關聯對應檔 查詢 此訂單明細所對應的MealBean-宗鈺
 	public abstract  MealBean getMealBean(OrderDetailBean bean);
 	
+	//新增訂單明細 - Noah
+	public abstract boolean insertOrderDetail(OrderDetailBean bean);
+	
+	//更改訂單明細 - Noah
+	public abstract boolean changeOrderDetail(OrderDetailBean bean);
+		
+	//刪除一筆訂單明細需要訂單明細ID - Noah
+	public abstract boolean deleteOrderDetail(Integer orderDetailID);
 }                           

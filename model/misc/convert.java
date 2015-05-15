7680ddb1d55d5d6523c15b09b231a6e4090bbb24
PasionDu <pvpms2412@gmail.com>
2015-05-15 19:00:25 +0800

    杜 convert

---
+++ model/misc/convert.java
@@ -0,0 +1,42 @@
+package model.misc;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class convert {
+
+	private static SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+	public static java.util.Date convertDate(String data) {
+		java.util.Date result = null;
+		try {
+			result = sdFormat.parse(data);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result = new java.util.Date(0);
+		}
+		return result;
+	}
+
+	public static double convertDouble(String data) {
+		double result = 0;
+		try {
+			result = Double.parseDouble(data);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			result = -1000;
+		}
+		return result;
+	}
+
+	public static int convertInt(String data) {
+		int result = 0;
+		try {
+			result = Integer.parseInt(data);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			result = -1000;
+		}
+		return result;
+	}
+	
+}

5dfeaf1b52ce49d04e23dc292881c6af8a8ba36f
GarysLin <cylin50@gmail.com>
2015-05-15 10:16:47 +0800

    更新Message

--- model/dao/MessageBoardDAO.java
+++ model/dao/MessageBoardDAO.java
@@ -6,9 +6,14 @@ import model.bean.MessageBoardBean;
 
 //Gary
 public interface MessageBoardDAO {
+	//查詢全部留言
 	public List<MessageBoardBean> select();
+	//查詢特定會員ID留言
 	public List<MessageBoardBean> select(int memberID);
+	//查詢特定留言
 	public MessageBoardBean selectOne(int messageID);
+	//新增留言
 	public boolean insert(MessageBoardBean bean);
+	//刪除特定留言
 	public boolean delete(int messageID);
 }

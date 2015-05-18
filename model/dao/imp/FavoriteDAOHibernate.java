553c187b4c9aa46293318ae939c0d57c7ff10dfc
steven <rsshppp@gmail.com>
2015-05-18 15:51:19 +0800

    Update FavoriteDAOHibernate() by Steven

--- model/dao/imp/FavoriteDAOHibernate.java
+++ model/dao/imp/FavoriteDAOHibernate.java
@@ -1,18 +1,22 @@
 package model.dao.imp;
 
+import java.util.Iterator;
 import java.util.List;
+
+import model.bean.FavoriteBean;
+import model.dao.FavoriteDAO;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
-import model.bean.AdvertisementBean;
-import model.bean.FavoriteBean;
-import model.bean.OwnerBean;
-import model.dao.FavoriteDAO;
-
 public class FavoriteDAOHibernate implements FavoriteDAO {
+	public FavoriteDAOHibernate() {
+
+	}
+
 	private SessionFactory sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
@@ -25,21 +29,40 @@ public class FavoriteDAOHibernate implements FavoriteDAO {
 
 	@Override
 	public boolean Create(FavoriteBean bean) {
-		this.getSession().save(bean);
-		return true;
+		if (bean != null) {
+			this.getSession().save(bean);
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public List<FavoriteBean> ReadAll(int memberID) {
-		Criteria criteria =this.getSession().createCriteria(FavoriteBean.class);
+		Criteria criteria = this.getSession()
+				.createCriteria(FavoriteBean.class);
 		criteria.add(Restrictions.eq("memberID", memberID));
-		List<FavoriteBean> list=criteria.list();
+		List<FavoriteBean> list = criteria.list();
 		return list;
 	}
 
 	@Override
 	public boolean Delete(int favoriteID) {
-		return false;
+		Session session = this.getSession();
+		Transaction tx = session.beginTransaction();
+		Criteria criteria = session.createCriteria(FavoriteBean.class);
+		criteria.add(Restrictions.eqOrIsNull("favoriteID", favoriteID));
+		Iterator<FavoriteBean> favoriteBeans = criteria.list().iterator();
+		if (favoriteBeans != null) {
+			while (favoriteBeans.hasNext()) {
+				FavoriteBean favoriteBean = (FavoriteBean) favoriteBeans.next();
+				session.delete(favoriteBean);
+			}
+			tx.commit();
+			session.close();
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
